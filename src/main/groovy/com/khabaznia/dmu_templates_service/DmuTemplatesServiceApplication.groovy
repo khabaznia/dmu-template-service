956b0bd0baed3b5a3ac14aa0489ac1fa498a2eb8
Dmytro Khabaznia <dmitry.kh1994@gmail.com>
2025-05-22 10:37:44 +0300

    Init dmu-template-service

---
+++ src/main/groovy/com/khabaznia/dmu_templates_service/DmuTemplatesServiceApplication.groovy
@@ -0,0 +1,13 @@
+package com.khabaznia.dmu_templates_service
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DmuTemplatesServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DmuTemplatesServiceApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/khabaznia/dmu_templates_service/DmuTemplatesServiceApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.khabaznia.dmu_templates_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DmuTemplatesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
